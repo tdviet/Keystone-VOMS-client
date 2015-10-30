f2fd681c47a422923356b9b5c1be930315a49ba6
Viet Tran <tdviet@gmail.com>
2015-10-30 14:11:35 +0100

    Initial test2

---
+++ keystone-voms.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+KEYSTONE="https://keystone3.ui.savba.sk:5000/v2.0/"
+VO="fedcloud.egi.eu"
+
+voms-proxy-info -e ||  voms-proxy-init --voms $VO -rfc
+
+curl -s --cert $X509_USER_PROXY -d '{"auth":{"voms": true}}' \
+     -H "Content-type:application/json" $KEYSTONE/tokens | \
+     python -c 'import sys, json; print json.load(sys.stdin)["access"]["token"]["id"]'
+
