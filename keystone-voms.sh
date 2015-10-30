72fe5d6a172962245f0b9aae5f4ac6450784ed25
Viet Tran <tdviet@gmail.com>
2015-10-30 15:04:54 +0100

    Add echo

--- keystone-voms.sh
+++ keystone-voms.sh
@@ -5,6 +5,8 @@ VO="fedcloud.egi.eu"
 
 voms-proxy-info -e ||  voms-proxy-init --voms $VO -rfc
 
+echo -n "The generated keystone token is: "
+
 curl -s --cert $X509_USER_PROXY -d '{"auth":{"voms": true}}' \
      -H "Content-type:application/json" $KEYSTONE/tokens | \
      python -c 'import sys, json; print json.load(sys.stdin)["access"]["token"]["id"]'
