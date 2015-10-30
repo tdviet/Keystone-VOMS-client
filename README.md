3b485775ada6a4e1ce331e1e812a1ea1633547c1
Viet Tran <tdviet@gmail.com>
2015-10-30 21:45:25 +0100

    Better debug information in case of error

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 This script will generated Keystone token from VOMS proxy certificate. No installation required.
 Just download and execute the script on the machine with your Grid certificate (voms-proxy-* command should be available there). 
+
+RedHat-based distribution may have problems with curl command due to NSS library
+
--- keystone-voms.sh
+++ keystone-voms.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+# Viet Tran
+# Institute of Informatcis SAS
+# Oct 2015
+
+# Change KEYSTONE and VO to your site if needed
 KEYSTONE="https://keystone3.ui.savba.sk:5000/v2.0/"
 CA_PATH="/etc/grid-security/certificates/"
 VO="fedcloud.egi.eu"
 
-
+# Check if VOMS proxy is valid, otherwise request new one
 voms-proxy-info -e ||  voms-proxy-init --voms $VO -rfc
 
-echo -n "The generated keystone token is: "
+# Get full token from Keystone
+token=$(curl -s -f --capath $CA_PATH --cert $X509_USER_PROXY -d '{"auth":{"voms": true}}' \
+        -H "Content-type:application/json" $KEYSTONE/tokens)
 
-curl -s --capath $CA_PATH --cert $X509_USER_PROXY -d '{"auth":{"voms": true}}' \
-     -H "Content-type:application/json" $KEYSTONE/tokens | \
-     python -c 'import sys, json; print json.load(sys.stdin)["access"]["token"]["id"]'
+if [ $? -eq 0 ]
+then
+  # Success, parsing the full token and print just token ID
+  echo -n "The generated keystone token is: "
+  echo $token | python -c 'import sys, json; print json.load(sys.stdin)["access"]["token"]["id"]'
+else
+  # Error, print debug info and exit
+  echo "Error while getting token. Please execute the following command to see full error message:"
+  echo curl -s --capath $CA_PATH --cert $X509_USER_PROXY -d '{"auth":{"voms": true}}' -H "Content-type:application/json" $KEYSTONE/tokens
+fi
+
 
